0738417d1a37168dd560d63f0047fd3626745bd1
coding <tiborprogrammer@yahoo.com>
2020-10-20 18:59:04 +0200

    Shader fixed.

--- src/Window.cpp
+++ src/Window.cpp
@@ -6,7 +6,7 @@
 
 // vertex = point/pos
 const char* vertexShader = R"(
-#version 400
+#version 330
 layout (location=0) in vec3 vp;
 void main() {
   gl_Position = vec4(vp, 1.0);
@@ -14,11 +14,11 @@ void main() {
 )";
 
 const char* colorShader = R"(
-#version 400
-out vec4 frag_colour;
+#version 330
+layout (location=0) out vec4 frag_colour;
 void main() {
   frag_colour = vec4(1.0, 0.2, 0.2, 1.0);
-};
+}
 )";
 
 void windowCloseCallback(GLFWwindow* glfwWindow) {
@@ -103,10 +103,12 @@ Window::Window(const WindowProperties &windowProperties) : windowProperties(wind
         return;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
     this->glfwwindow = glfwCreateWindow(windowProperties.width, windowProperties.height, windowProperties.title.c_str(), nullptr, nullptr);
 
     std::cout << "Creating window..." << std::endl;
@@ -143,18 +145,17 @@ Window::Window(const WindowProperties &windowProperties) : windowProperties(wind
 
     float vertexes[3][3] = {{-0.5f, -0.5f, 0.0f}, {0.5f, -0.5f, 0.0f}, {0.5f,  0.5f, 0.0f}};
 
-    unsigned int bufferId;
+    GLuint bufferId;
     glGenBuffers(1, &bufferId);
     glBindBuffer(GL_ARRAY_BUFFER, bufferId);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*3, vertexes, GL_STATIC_DRAW);
 
     glGenVertexArrays(1, &vertexArrayId);
     glBindVertexArray(vertexArrayId);
-    glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, bufferId);
     // 0 = current buffer, number of dimensions in a vertex, type of positions, coordinate type(-1 to 1), size of each vertex(0 = auto)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
+    glEnableVertexAttribArray(0);
 
     GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShaderId, 1, &vertexShader, nullptr);
@@ -162,9 +163,9 @@ Window::Window(const WindowProperties &windowProperties) : windowProperties(wind
     GLuint colorShaderId = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(colorShaderId, 1, &colorShader, nullptr);
 
-    GLint val1, val2;
-    glGetShaderiv(colorShaderId, GL_COMPILE_STATUS, &val1);
-    glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &val2);
+   // GLint val1, val2;
+    //glGetShaderiv(colorShaderId, GL_COMPILE_STATUS, &val1);
+    //glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &val2);
 
     glCompileShader(vertexShaderId);
     glCompileShader(colorShaderId);
@@ -173,8 +174,8 @@ Window::Window(const WindowProperties &windowProperties) : windowProperties(wind
     glAttachShader(this->shaderProgramId, vertexShaderId);
     glAttachShader(this->shaderProgramId, colorShaderId);
 
-    glLinkProgram(this->shaderProgramId);
 
+    glLinkProgram(this->shaderProgramId);
 
 
     /*unsigned int indicesArray[] = {0, 1, 2, 3};
@@ -182,8 +183,6 @@ Window::Window(const WindowProperties &windowProperties) : windowProperties(wind
     glGenBuffers(1, &indicesBufferId);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesBufferId);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*4, vertexes, GL_STATIC_DRAW);*/
-
-    GLenum error_code = glGetError();
 }
 
 void Window::setEventProcessingFn(const EventProcessingFn &eventProcessingFn) {
@@ -198,9 +197,6 @@ void Window::update() {
     glUseProgram(this->shaderProgramId);
     glBindVertexArray(vertexArrayId);
     glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-    GLenum error_code = glGetError();
 
     // Swap front and back buffers
     glfwSwapBuffers(this->glfwwindow);
